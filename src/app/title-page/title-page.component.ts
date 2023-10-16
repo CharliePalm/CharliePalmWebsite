9f049095460c1d73e9d480ce908b1c599e9f4846
Charles Palm <charles.palm@tasconline.com>
2023-10-16 13:51:36 -0500

    plx working, working on adding fade ins

--- src/app/title-page/title-page.component.ts
+++ src/app/title-page/title-page.component.ts
@@ -2,15 +2,19 @@ import { AfterViewInit, Component, EventEmitter, HostListener, Input, OnInit, Ou
 import { MatSidenav } from '@angular/material/sidenav';
 import { DataLoader } from '../../assets/data/data';
 import { State } from '../app.model';
+import { Subscription, fromEvent } from 'rxjs';
+import { trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-title-page',
   templateUrl: './title-page.component.html',
-  styleUrls: ['./title-page.component.scss']
+  styleUrls: ['./title-page.component.scss'],
 })
-export class TitlePageComponent implements OnInit {
+export class TitlePageComponent implements OnInit, AfterViewInit {
   @Output() public goToState: EventEmitter<State> = new EventEmitter<State>();
   @Input() public dataLoader!: DataLoader;
+  public eventSubscription!: Subscription;
+  public images = document.getElementsByClassName('bg-img');
   public state = State;
   public isCollapsed = false;
 
@@ -19,6 +23,15 @@ export class TitlePageComponent implements OnInit {
   }
   
   ngOnInit() {
+    document.querySelector('.parallax-container')?.addEventListener('wheel', (e) => {
+        //e.preventDefault(); // Prevent default scroll behavior
+        this.onWindowScroll();
+    });
+    this.windowSizeCheck();
+  }
+
+  ngAfterViewInit(): void {
+    this.onWindowScroll();
     this.windowSizeCheck();
   }
 
@@ -26,7 +39,7 @@ export class TitlePageComponent implements OnInit {
     console.log('test');
   }
 
-  public scroll(el: HTMLElement) {
+  public viewElement(el: HTMLElement) {
     console.log('test');
     el.scrollIntoView();
   }
@@ -36,6 +49,19 @@ export class TitlePageComponent implements OnInit {
       this.isCollapsed = true;
     } else {
       this.isCollapsed = false;
+    }
+  }
+
+  onWindowScroll() {
+    for (let i = 0; i < this.images.length; i++) {
+      const image = this.images.item(i);
+      const rect = image?.getBoundingClientRect();
+      console.log(rect);
+      const isVisible = ((rect?.top ?? 0) >= 0 && (rect?.bottom ?? 0) <= window.innerHeight);
+      console.log(isVisible);
+      if (isVisible) {
+        image?.classList.add('visible');
+      }
     }
   }
 }
